0124d796a291a5f298acecd2dceb590c7dcd5b66
w1ld-resonation <no_nombre_otro@icloud.com>
2026-02-27 14:38:04 -0300

    Revert "Fix: Added missing 'cart' translation for mobile header in RU and EN."
    This reverts commit d24409b7383ed965b81303b1c817035a0fd6901a.

--- packages/Webkul/Shop/src/Resources/lang/en/app.php
+++ packages/Webkul/Shop/src/Resources/lang/en/app.php
@@ -536,7 +536,6 @@ return [
                     'back-button' => 'Back to Main Menu',
                     'bagisto' => 'Bagisto',
                     'compare' => 'Compare',
-                    'cart' => 'Cart',
                     'currencies' => 'Currencies',
                     'dropdown-text' => 'Manage Cart, Orders & Wishlist',
                     'locales' => 'Locales',
