2236ec213ef9abf5a2c8c2ea95774449a8feea48
w1ld-resonation <no_nombre_otro@icloud.com>
2026-02-28 18:53:08 -0300

    chore: remove debug/scratch files from repo

--- find_jetton.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('ton_events.json') as f:
-    data = json.load(f)
-
-for event in data.get('events', []):
-    for action in event.get('actions', []):
-        if action['type'] == 'JettonTransfer':
-            print(json.dumps(action, indent=2))
