20b267aa426a7d98ba2a0c4832bd4cee7fd27399
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-02 09:52:39 -0300

    ui(crypto): make wallet edit icon always visible

--- packages/Webkul/Shop/src/Resources/views/customers/account/crypto/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/crypto/index.blade.php
@@ -171,8 +171,8 @@
                                               onclick="const newAlias = prompt('Введите новое название кошелька', '{{ $address->alias ?? '' }}'); if (newAlias !== null) { document.getElementById('update-alias-form-{{ $address->id }}').querySelector('input[name=alias]').value = newAlias; document.getElementById('update-alias-form-{{ $address->id }}').submit(); }">
                                             {{ $fullAlias }}
                                         </span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 text-zinc-300 cursor-pointer hover:text-violet-400 opacity-0 group-hover/alias:opacity-100 transition-opacity" viewBox="0 0 20 20" fill="currentColor"
-                                             onclick="const newAlias = prompt('Введите новое название кошелька', '{{ $address->alias ?? '' }}'); if (newAlias !== null) { document.getElementById('update-alias-form-{{ $address->id }}').querySelector('input[name=alias]').value = newAlias; document.getElementById('update-alias-form-{{ $address->id }}').submit(); }">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 text-zinc-400 cursor-pointer hover:text-violet-500 transition-colors" viewBox="0 0 20 20" fill="currentColor"
+                                             onclick="const newAlias = prompt('Введите новое название кошелька', '{{ addslashes($address->alias ?? '') }}'); if (newAlias !== null) { document.getElementById('update-alias-form-{{ $address->id }}').querySelector('input[name=alias]').value = newAlias; document.getElementById('update-alias-form-{{ $address->id }}').submit(); }">
                                             <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                         </svg>
                                         <form id="update-alias-form-{{ $address->id }}" action="{{ route('shop.customers.account.crypto.update_alias', $address->id) }}" method="POST" class="hidden">
