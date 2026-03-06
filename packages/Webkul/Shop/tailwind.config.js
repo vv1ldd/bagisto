8097ebdf08ccb0b667cb26b852f4959e30318ea7
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-05 21:47:06 -0300

    Implement global sharp corners style for Shop and Admin

--- packages/Webkul/Shop/tailwind.config.js
+++ packages/Webkul/Shop/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
     content: ["./src/Resources/**/*.blade.php", "./src/Resources/**/*.js"],
 
     theme: {
+        borderRadius: {
+            'none': '0',
+            'sm': '0',
+            DEFAULT: '0',
+            'md': '0',
+            'lg': '0',
+            'xl': '0',
+            '2xl': '0',
+            '3xl': '0',
+            'full': '0',
+        },
+
         container: {
             center: true,
 
