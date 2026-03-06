cbf4e81c96aacae4dd65d39ecef45c7a2d7bbfb0
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-06 11:25:43 -0300

    [UI/UX] Move product title into tile and shrink action buttons on PDP

--- packages/Webkul/Shop/src/Resources/views/components/shimmer/products/view.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/products/view.blade.php
@@ -1,7 +1,6 @@
 <div class="w-full max-w-[700px] mx-auto px-4 lg:px-6 space-y-10 pt-6">
-    <!-- Back + Title Shimmer -->
-    <div class="flex items-center justify-between gap-4">
-        <div class="shimmer h-8 w-2/3 "></div>
+    <!-- Back Shimmer -->
+    <div class="flex items-center justify-end">
         <div class="shimmer w-10 h-10 "></div>
     </div>
 
@@ -15,6 +14,7 @@
 
         <!-- RIGHT: Info Shimmer -->
         <div class="flex flex-col gap-4 p-6 max-sm:p-4 flex-1 min-w-0 bg-white relative">
+            <div class="shimmer h-8 w-2/3 mb-2"></div>
             <div class="space-y-4">
                 <div class="flex flex-wrap gap-2">
                     <div class="shimmer h-6 w-20 "></div>
@@ -23,8 +23,8 @@
             </div>
 
             <div class="flex flex-col gap-3">
-                <div class="shimmer h-14 w-full "></div>
-                <div class="shimmer h-14 w-full "></div>
+                <div class="shimmer h-12 w-full "></div>
+                <div class="shimmer h-12 w-full "></div>
             </div>
 
             <div class="border-t border-zinc-100"></div>
