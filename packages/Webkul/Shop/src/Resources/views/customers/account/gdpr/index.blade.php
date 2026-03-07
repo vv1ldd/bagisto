3d5aa7ef20da86bb899417700166eae1ee92ca61
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-07 12:03:26 -0300

    Fix profile close button position and restrict footer links to home page

--- packages/Webkul/Shop/src/Resources/views/customers/account/gdpr/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/gdpr/index.blade.php
@@ -13,7 +13,7 @@
 
         <div class="mx-4 flex-auto relative bg-white border border-gray-100 pb-8">
             <a href="javascript:window.history.length > 1 ? window.history.back() : window.location.href = '{{ route('shop.customers.account.index') }}'"
-                class="absolute top-4 right-4 z-10 w-8 h-8 bg-white border border-gray-100 flex items-center justify-center text-zinc-400 active:scale-95 transition-all hover:text-[#7C45F5] hover:border-gray-200">
+                class="absolute top-5 right-5 z-20 w-8 h-8 bg-white border border-gray-100 flex items-center justify-center text-zinc-400 active:scale-95 transition-all hover:text-[#7C45F5] hover:border-gray-200">
                 <span class="icon-cancel text-xl"></span>
             </a>
 
--- packages/Webkul/Shop/src/Resources/views/customers/account/login-activity/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/login-activity/index.blade.php
@@ -11,7 +11,7 @@
 
         <div class="flex-1 px-8 pt-6 pb-20 max-md:px-5 relative">
             <a href="javascript:window.history.length > 1 ? window.history.back() : window.location.href = '{{ route('shop.customers.account.index') }}'"
-                class="absolute top-4 right-4 z-10 w-8 h-8 bg-white border border-gray-100 flex items-center justify-center text-zinc-400 active:scale-95 transition-all hover:text-[#7C45F5] hover:border-gray-200">
+                class="absolute top-5 right-5 z-20 w-8 h-8 bg-white border border-gray-100 flex items-center justify-center text-zinc-400 active:scale-95 transition-all hover:text-[#7C45F5] hover:border-gray-200">
                 <span class="icon-cancel text-xl"></span>
             </a>
 
