c6815018f5709e8e9ae76b048f1df8191faa6220
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-10 20:24:41 -0300

    Decouple backend and frontend WebSocket connections to fix Docker 500 Broadcast Error

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,13 +35,20 @@ return [
             'secret' => env('REVERB_APP_SECRET'),
             'app_id' => env('REVERB_APP_ID'),
             'options' => [
-                'host' => env('REVERB_HOST'),
-                'port' => env('REVERB_PORT', 443),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'host' => env('REVERB_SERVER_HOST', 'reverb'),
+                'port' => env('REVERB_SERVER_PORT', 8088),
+                'scheme' => env('REVERB_SERVER_SCHEME', 'http'),
+                'useTLS' => env('REVERB_SERVER_SCHEME', 'http') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'verify' => false,
+            ],
+            // Custom keys isolated for the frontend browser
+            'frontend' => [
+                'host' => env('REVERB_HOST', '127.0.0.1'),
+                'port' => env('REVERB_PORT', 8088),
+                'scheme' => env('REVERB_SCHEME', 'http'),
             ],
         ],
 
