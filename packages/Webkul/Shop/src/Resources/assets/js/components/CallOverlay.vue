b7bd4ce589a5ee3683876225f5c58c67ebe9a3ef
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-17 13:07:31 -0300

    WebRTC: Add domestic Russian STUN servers for connection resilience

--- packages/Webkul/Shop/src/Resources/assets/js/components/CallOverlay.vue
+++ packages/Webkul/Shop/src/Resources/assets/js/components/CallOverlay.vue
@@ -355,7 +355,9 @@ export default {
                 iceServers: [
                     { urls: 'stun:stun.meanly.ru:3478' },
                     { urls: 'stun:stun.l.google.com:19302' },
-                    { urls: 'stun:stun1.l.google.com:19302' },
+                    { urls: 'stun:stun.miwifi.com:3478' }, // Xiaomi - Good global/RU compatibility
+                    { urls: 'stun:stun.voip.ru:3478' },     // Domestic RU
+                    { urls: 'stun:stun.sipnet.net:3478' },  // Domestic RU
                 ],
                 iceCandidatePoolSize: 2
             },
