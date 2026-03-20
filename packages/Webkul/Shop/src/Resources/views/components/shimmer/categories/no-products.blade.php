50477a051513aea9595bee2b3a3074aee6c05f27
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-20 17:07:57 -0300

    Fix: Actually track the renamed no-products component

---
+++ packages/Webkul/Shop/src/Resources/views/components/shimmer/categories/no-products.blade.php
@@ -0,0 +1,9 @@
+<div class="m-auto grid w-full place-content-center items-center justify-items-center py-32 text-center">
+    <!-- Icon Placeholder -->
+    <div class="mb-4 flex h-24 w-24 items-center justify-center bg-zinc-100 text-zinc-300 shimmer">
+        <div class="h-14 w-14 bg-zinc-200"></div>
+    </div>
+
+    <!-- Text Placeholder -->
+    <div class="h-6 w-48 bg-zinc-100 shimmer mt-4"></div>
+</div>
