8862510d03f49a46ff95fe0cdc4f9b2fcc0d8937
w1ld-resonation <no_nombre_otro@icloud.com>
2026-03-21 07:33:13 -0300

    Fix: Restore layout to account index to fix broken UI

--- packages/Webkul/Shop/src/Resources/views/customers/account/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/index.blade.php
@@ -1,3 +1,8 @@
+<x-shop::layouts.account>
+    <x-slot:title>
+        Личный кабинет
+    </x-slot>
 
     {{-- Security Section --}}
     @include('shop::customers.account.security')
+</x-shop::layouts.account>
