07ba8cda098dd359e4f9010bc967ae8ac336991a
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-02 19:44:53 -0300

    Cleanup: Remove the entire legacy blockchain development directory (Hardhat project and node_modules) for production deployment.

--- parse_events.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('blockchain/data/ton_events.json') as f:
-    data = json.load(f)
-
-for event in data.get('events', []):
-    for action in event.get('actions', []):
-        print(action['type'])
