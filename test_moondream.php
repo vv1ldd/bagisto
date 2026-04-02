f91f4891fadd616b5cde3b58a9d45d18de029005
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-02 19:42:22 -0300

    Cleanup: Remove all diagnostic, test and debugging scripts from root and public directories for production.

--- test_moondream.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Simple script to test if moondream can extract from the provided image
-$imgBase64 = base64_encode(file_get_contents('/Users/w1ld/Downloads/bagisto-2.3 2/test_image_placeholder.jpg')); 
-// We don't have the actual image on disk, so this is just a conceptual test script 
-// I'll actually rely on the user's feedback or ask them.
-echo "Script created";
