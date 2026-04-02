02e1488303bab6602bc79f3a54897f68f4ce4735
w1ld-resonation <no_nombre_otro@icloud.com>
2026-04-02 19:48:05 -0300

    Cleanup: Backup smart contracts to _backups/ and remove root-level diagnostic PHP/JS/HTML files.

--- test_ton_dest.js
+++
@@ -1,9 +0,0 @@
-const dest = 'UQBTW0bj2lR0NQ2WdlRVyuLNNO55uokEo9hauUMWOArxAKVx';
-fetch(`https://tonapi.io/v2/accounts/${dest}/jettons/history?limit=10`, {
-    headers: { 'Accept': 'application/json' }
-}).then(async res => {
-    console.log("Status:", res.status);
-    const text = await res.text();
-    console.log("Raw Response:");
-    console.log(text);
-}).catch(console.error);
